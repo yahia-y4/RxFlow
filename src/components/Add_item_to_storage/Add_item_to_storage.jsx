b3c9ef609e4de2e355fc59c12cea289e53bb9598
yahia-y4 <yalhmwd994@gmail.com>
2025-05-25 19:51:05 +0300

    4

---
+++ src/components/Add_item_to_storage/Add_item_to_storage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Add_item_to_storage.css"
+import Input from "../../components/Input/Input";
+
+export default function Add_item_to_storage(){
+    return(
+         <div className="Add_item_to_storage_div">
+           
+               <Input label_v={" الاسم التجاري"} id_v={"add_name_item_input"}></Input>
+               <Input label_v={"اسم الشركة "} id_v={"add_co_name_item_input"}></Input>
+               <Input label_v={"الشكل الصيدلاني"} id_v={""}></Input>
+               <Input label_v={"التركيز "} id_v={""}></Input>
+               <Input label_v={"العيار"} id_v={""}></Input>
+                <Input label_v={"العبوة "} id_v={""}></Input>
+                 <Input label_v={"العدد "} id_v={""}></Input>
+                 <Input label_v={"سعر القطعة"} id_v={""}></Input>
+                 <Input label_v={"نسبة الربح %"} id_v={""}></Input>
+                 <Input label_v={"الباركود "} id_v={""}></Input>
+                 <Input label_v={"الصنف "} id_v={""}></Input>
+            
+
+           </div>
+    )
+}
--- src/pages/storage/right_part_srorage/Right_part_storage.jsx
+++ src/pages/storage/right_part_srorage/Right_part_storage.jsx
@@ -1,33 +1,17 @@
 import React from "react";
-import "./Right_part_storage.css"
+import "./Right_part_storage.css";
 
 import Insideicon from "../../../components/InsideIcon/Insideicon";
-import Input from "../../../components/Input/Input";
+import Add_item_to_storage from "../../../components/Add_item_to_storage/Add_item_to_storage";
 
- export default function Right_part_storage(){
-    return (
-        <div id="right_part_storage_div">
-           <div className="top_icons_right_flex">
-            <Insideicon id_i="add" class_f="fas" font="&#xf108;"></Insideicon>
-            
-           </div>
+export default function Right_part_storage() {
+  return (
+    <div id="right_part_storage_div">
+      <div className="top_icons_right_flex">
+        <Insideicon id_i="add" class_f="fas" font="&#xf108;"></Insideicon>
+      </div>
 
-           <div className="add_inputs_right_div">
-           
-               <Input label_v={" الاسم التجاري"} id_v={"add_name_item_input"}></Input>
-               <Input label_v={"اسم الشركة "} id_v={"add_co_name_item_input"}></Input>
-               <Input label_v={"الشكل الصيدلاني"} id_v={""}></Input>
-               <Input label_v={"التركيز "} id_v={""}></Input>
-               <Input label_v={"العيار"} id_v={""}></Input>
-                <Input label_v={"العبوة "} id_v={""}></Input>
-                 <Input label_v={"العدد "} id_v={""}></Input>
-                 <Input label_v={"سعر القطعة"} id_v={""}></Input>
-                 <Input label_v={"نسبة الربح %"} id_v={""}></Input>
-                 <Input label_v={"الباركود "} id_v={""}></Input>
-                 <Input label_v={"الصنف "} id_v={""}></Input>
-            
-
-           </div>
-        </div>
-    )
- }
+      <Add_item_to_storage></Add_item_to_storage>
+    </div>
+  );
+}
